4252649c434a81d36f8c0219d3bef22d17eca96f
Zönk <112422609+Zonkrur@users.noreply.github.com>
2023-04-07 19:30:28 +0200

    Played around with destructables
    Finetuned the basics of destructables being destroyed and eventually respawning after a timer

--- Assets/Scripts/Destructables.cs
+++ Assets/Scripts/Destructables.cs
@@ -6,19 +6,35 @@ public class Destructables : MonoBehaviour, IDamageable
 {
     [SerializeField] private GameObject Partial;
     [SerializeField] private GameObject Destroyed;
-    public float health;
+    [SerializeField] private Renderer render;
+    [SerializeField] private Renderer partialRender;
+    [SerializeField] private Collider collider;
+    private float health;
     public float partial_health;
+    [SerializeField] private float deathLength;
+    private float timer;
+    [SerializeField] public float maxHealth;
+    private bool dead;
+    ObjectRemover remover;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = deathLength;
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dead)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                respawn();
+            }
+        }
     }
 
     public bool TryToDestroyDamageable(int damage, Spell.SpellType? spellType)
@@ -27,12 +43,27 @@ public class Destructables : MonoBehaviour, IDamageable
         if (health <= partial_health)
         {
             Partial.SetActive(true);
+            render.enabled = false;
         }
         if (health <= 0)
         {
-            Instantiate(Destroyed, transform.position, transform.rotation);
-            Destroy(gameObject); }
+            
+            remover = Instantiate(Destroyed, transform.position, transform.rotation).GetComponent<ObjectRemover>();
+            Partial.SetActive(false);
+            collider.enabled = false;
+            dead = true;
+        }
         return false; }
+
+    private void respawn()
+    {
+        dead = false;
+        render.enabled = true;
+        collider.enabled = true;
+        health = maxHealth;
+        timer = deathLength;
+        remover.RemoveObject();
+    }
     
     public void KnockBack(float knockBack)
     { }
